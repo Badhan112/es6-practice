95c4b0bdfd445c2196f34be2e867d377f8c8885a
Badhan112 <badhancsesastc112@gmail.com>
2021-02-01 13:35:20 +0600

    let and const

---
+++ letConst.js
@@ -0,0 +1,36 @@
+//Constant
+const baby = "Chalantica Rani Progoti";
+//baby = "Puja";
+console.log(baby);
+
+const num = [23, 34];
+console.log(num);
+
+num[1] = 55;
+console.log(num);
+
+num.push(112);
+console.log(num);
+
+// num = ["Badhan", "Puja"];
+
+const developer = {name: "Badhan", id: 112};
+console.log(developer);
+
+developer.id = 1720112;
+console.log(developer);
+
+//let (poriborton korte dibo)
+let clint = "David";
+console.log(clint);
+
+clint = "Jhon";
+console.log(clint);
+
+//let is a scope variable
+let sum;
+for(let i = 0; i<=10; i++){
+    sum = sum + i;
+}
+
+//console.log(i);
