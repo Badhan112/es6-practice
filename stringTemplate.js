14dcccb745e7de3d6cd2d304acfe5a216ad33a39
Badhan112 <badhancsesastc112@gmail.com>
2021-02-01 22:20:19 +0600

    String Template with `(backtick)

---
+++ stringTemplate.js
@@ -0,0 +1,19 @@
+const firstName = "Badhan";
+const lastName = "Chandra Barman";
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//ES6
+const fullName2 = `${firstName} ${lastName} is a Programmer. Age is ${18+7}`;
+console.log(fullName2);
+
+const multiline = "I am Badhan\n"
+                + "I am a Programmer\n"
+                + "Professional Web Developer & Mobile Application Developer";
+console.log(multiline);
+
+//ES6
+const multiline2 = `I am Badhan
+I am a Programmer
+I am a Professional Web Developer & Mobile Application Developer`;
+console.log(multiline2);
